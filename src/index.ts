6e914d544a5c7d669960282f30d4d886e99c3bcc
KanonKC <kanonkce@gmail.com>
2025-06-04 20:55:07 +0700

    Change 'Users' to 'DiscordUser'

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,11 @@ import { BaseInteraction } from "discord.js";
 import { SlashCommandObject } from "./scripts/types/SlashCommandObject";
 import { slashCommandList } from "./commands";
 import { getSlashCommandObject } from "./utils/slash-command";
-import UsersService from "./services/users.service";
+import DiscordUsersService from "./services/discordUser.service";
 
 dotenv.config();
 let commands: SlashCommandObject;
-const us = new UsersService();
+const us = new DiscordUsersService();
 
 const client = new Client({
 	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildIntegrations],
@@ -20,7 +20,7 @@ client.once(Events.ClientReady, async (client) => {
 });
 
 client.on("interactionCreate", async (interaction: BaseInteraction) => {
-    await us.createIfUserNotExists(interaction.user.id);
+    await us.createIfDiscordUserNotExists(interaction.user.id);
 	if (interaction.isChatInputCommand()) {
 		await commands[interaction.commandName].onCommandExecuted(interaction);
 	} else if (interaction.isButton()) {
