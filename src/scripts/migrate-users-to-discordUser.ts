bf46d2c0884972bcd1bbbd4f8dd39f51128775e9
KanonKC <kanonkce@gmail.com>
2025-06-05 20:50:41 +0700

    Add statistics command

--- src/scripts/migrate-users-to-discordUser.ts
+++ src/scripts/migrate-users-to-discordUser.ts
@@ -1,12 +1,27 @@
 import prisma from "../databases";
 
 export async function migrateDiscordUsersToDiscordUser() {
-    const discordUsers = await prisma.discordUser.findMany();
-    for (const discordUser of discordUsers) {
-        await prisma.discordUser.create({
-            data: discordUser,
-        });
-    }
+	const discordUsers = await prisma.discordUser.findMany();
+	for (const discordUser of discordUsers) {
+		await prisma.discordUser.upsert({
+			where: {
+				discordId: discordUser.discordId,
+			},
+			update: {
+				point: discordUser.point,
+				lastClaimedAt: discordUser.lastClaimedAt,
+				createdAt: discordUser.createdAt,
+				updatedAt: discordUser.updatedAt,
+			},
+			create: {
+				discordId: discordUser.discordId,
+				point: discordUser.point,
+				lastClaimedAt: discordUser.lastClaimedAt,
+				createdAt: discordUser.createdAt,
+				updatedAt: discordUser.updatedAt,
+			},
+		});
+	}
 }
 
 migrateDiscordUsersToDiscordUser();
