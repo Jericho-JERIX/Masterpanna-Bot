6cccf5c0a7f49882c13b241b9232bb0579a32e15
KanonKC <kanonkce@gmail.com>
2025-06-05 20:50:41 +0700

    Add statistics command

--- src/commands.ts
+++ src/commands.ts
@@ -3,10 +3,12 @@ import { HourlyReward } from "./commands/hourly-reward";
 import { Leaderboards } from "./commands/admins/leaderboards";
 import { Point } from "./commands/point";
 import { SlashCommand } from "./scripts/types/SlashCommand";
+import { Statistics } from "./commands/admins/statistics";
 
 export const slashCommandList: SlashCommand[] = [
-  HourlyReward,
-  Point,
-  ManagePoint,
-  Leaderboards,
+	HourlyReward,
+	Point,
+	ManagePoint,
+	Leaderboards,
+	Statistics,
 ];
---
+++ src/commands/admins/statistics.ts
@@ -0,0 +1,66 @@
+import { GuildMember, SlashCommandBuilder } from "discord.js";
+import { SlashCommand } from "../../scripts/types/SlashCommand";
+import AdminService from "../../services/admin.service";
+import DiscordUserService from "../../services/discordUser.service";
+import { StatisticsEmbed } from "../../components/embeds/StatisticsEmbed";
+
+export const Statistics: SlashCommand = {
+	slashCommandBuilder: new SlashCommandBuilder()
+		.setName("statistics")
+		.setDescription("แสดงข้อมูลสถิติของการใช้งานของผู้ใช้งาน"),
+
+	async onCommandExecuted(interaction) {
+		const us = new DiscordUserService();
+		const as = new AdminService();
+
+		if (!as.isGuildMemberIsAdmin(interaction.member as GuildMember)) {
+			await interaction.reply({
+				content: "คุณไม่มีสิทธิในการใช้งานคำสั่งนี้",
+				ephemeral: true,
+			});
+			return;
+		}
+
+		const discordUserList = await us.getDiscordUserList();
+
+		const totalPoint = discordUserList.reduce(
+			(acc, user) => acc + user.point,
+			0
+		);
+		const totalUser = discordUserList.length;
+
+		const averagePoint = totalPoint / totalUser;
+		const maxPoint = Math.max(...discordUserList.map((user) => user.point));
+		const minPoint = Math.min(...discordUserList.map((user) => user.point));
+
+		const maxUser = discordUserList.find((user) => user.point === maxPoint);
+		const minUser = discordUserList.find((user) => user.point === minPoint);
+
+		if (!maxUser || !minUser) {
+			await interaction.reply({
+				content: "เกิดข้อผิดพลาดในการดึงข้อมูลสถิติ",
+				ephemeral: true,
+			});
+			return;
+		}
+
+		const sd = Math.sqrt(
+			discordUserList.reduce(
+				(acc, user) => acc + Math.pow(user.point - averagePoint, 2),
+				0
+			) / totalUser
+		);
+
+		await interaction.reply({
+			embeds: [
+				StatisticsEmbed({
+					mean: averagePoint,
+					maxUser,
+					minUser,
+					sd,
+				}),
+			],
+			// ephemeral: true,
+		});
+	},
+};
