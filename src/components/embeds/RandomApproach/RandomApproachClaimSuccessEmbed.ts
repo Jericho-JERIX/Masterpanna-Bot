83941adb31cbe132cb833570a2a789f0cbfab18c
KanonKC <kanonkce@gmail.com>
2025-06-20 03:20:24 +0700

    Random Approach: Fastest claimed

--- src/components/embeds/RandomApproach/RandomApproachClaimSuccessEmbed.ts
+++ src/components/embeds/RandomApproach/RandomApproachClaimSuccessEmbed.ts
@@ -1,18 +1,28 @@
 import { EmbedBuilder } from "discord.js";
 import { coin } from "../../../utils/ui";
 import RandomApproachConstant from "../../../constants/random-approach.constant";
+import { DiscordUser, RandomApproach } from "../../../../generated/prisma";
 
 export function RandomApproachClaimSuccessEmbed({
     discordId,
     rewardPoints,
     point,
     timeDiffMs,
+    fastestRa,
+    fastestDiscordUser,
 }: {
     discordId: string;
     rewardPoints: number;
     point: number;
     timeDiffMs: number;
+    fastestRa?: RandomApproach | null;
+    fastestDiscordUser?: DiscordUser | null;
 }) {
+    let fastestMessage = "";
+    if (fastestRa && fastestDiscordUser) {
+        const timeDiffMs = fastestRa.claimedAt!.getTime() - fastestRa.createdAt.getTime();
+        fastestMessage = `\n\n*🔥 เร็วที่สุดตอนนี้คือ \`${timeDiffMs/1000}\` วินาที โดย <@${fastestDiscordUser.discordId}>*`;
+    }
 	return new EmbedBuilder()
     .setAuthor({ name: RandomApproachConstant.Title })
     .setColor(RandomApproachConstant.Color)
@@ -20,7 +30,7 @@ export function RandomApproachClaimSuccessEmbed({
     .setDescription(
         `<@${discordId}> ได้รับ ${coin(
             rewardPoints
-        )} แต้ม โดยใช้เวลา \`${timeDiffMs/1000}\` วินาที`
+        )} แต้ม โดยใช้เวลา \`${timeDiffMs/1000}\` วินาที${fastestMessage}`
     )
     .setFooter({
         text: `ตอนนี้คุณมี ${point} แต้ม`,
