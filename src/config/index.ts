b679c2efd5e9cf010cfe8218857cad034227ea98
KanonKC <kanonkce@gmail.com>
2025-06-25 03:08:27 +0700

    Auto-assign Player Role

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,12 +3,13 @@ import { readFileSync } from "fs";
 export interface Config {
 	adminUserIdList: string[];
 	adminRoleIdList: string[];
-    timeFormat: string;
-    timezone: string;
-    channelId: string;
+	timeFormat: string;
+	timezone: string;
+	channelId: string;
 	hourlyReward: {
 		cooldown: number;
-	};  
+	};
+	playerRoleId: string;
 }
 
 const data = readFileSync("./config.json", "utf-8");
